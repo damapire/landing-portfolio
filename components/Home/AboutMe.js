2d4392bcafdf0305e9541b9706caf0e834d2e227
Daniel Pinto <danielpinto.ml@gmail.com>
2024-02-15 22:12:16 -0500

    Add basic and initial project structure

---
+++ components/Home/AboutMe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutMe () {
+  return (
+    <section>
+      <h1>About me</h1>
+    </section>
+  )
+}
---
+++ components/Home/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Hero () {
+  return (
+    <section className='container-fluid hero'>
+      <div className='row'>
+        <div className='col-6'>
+          <h1 className='hero__title'>Hey There, I'm Daniel</h1>
+          <a href='' className='d-none d-lg-block hero__email'>DANIEL.DEV@OUTLOOK.COM</a>
+        </div>
+        <div className='col-6'>
+          <p>I design beautifully simple things, And I love what i do</p>
+        </div>
+      </div>
+
+    </section>
+  )
+}
