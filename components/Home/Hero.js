28b0a8ec072c24db7a660c850d2a18f976687701
Daniel Pinto <danielpinto.ml@gmail.com>
2024-02-24 20:26:58 -0500

    conflics resolve branches

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -7,25 +7,28 @@ import IMG_DEV from '@/public/assets/home/avatar-dev.png'
 
 export default function Hero () {
   return (
-    <section className='container-fluid hero'>
-      <div className='row'>
-        <section className='col-lg-6'>
-          <h1 className='hero__title'>Hey There, I’m Daniel Pinto</h1>
+    <section className='container-fluid hero d-lg-flex align-items-center'>
+      <div className='row d-lg-flex align-items-center hero__width'>
+        <section className='row col-lg-6 '>
+          <h1 className='hero__title'>Hey There, I’m Daniel</h1>
           <Link href='#' className='d-none d-lg-block hero__email'>danipinto.dev@gmail.com</Link>
-          <div className='hero__experiencies'>
-            <strong>3</strong>
-            <p>Years Experiences</p>
+          <div className='d-none d-lg-block'>
+            <div className="hero__experiencies">
+              <strong className='hero__experiencies-number'>3</strong>
+              <p className='hero__experiencies-years'>Years Experiences</p>
+            </div>
           </div>
         </section>
+
         <section className='col-lg-6 position-relative'>
           <p className='hero__subtitle'>I design beautifully simple things, <br/> And I love what i do</p>
           <figure className='hero__badge p-0'>
             <Image src={BADGE_MINTIC} alt='Badge Mintic' />
           </figure>
-          <div className='mx-auto text-center'>
+          <div className='mx-auto my-0 text-center'>
             <Image src={IMG_DEV} width={350} height={400} alt='Foto Dev' className='hero__photo mx-auto' />
           </div>
-          <p className='d-none d-md-block'>FullStack JavaScript Developer</p>
+          <p className='hero__position d-none d-md-block'>FullStack JavaScript Developer</p>
         </section>
       </div>
     </section>
